2549e14699694e53bce38dac0db97b6b0c8e3ed3
Yongwook Kim <99084710+yongwookkkkim@users.noreply.github.com>
2023-03-16 08:08:44 +0900

    Update angles.ino

--- angles.ino
+++ angles.ino
@@ -1,7 +1,14 @@
 int theta1(int x, int y, int z){
-  return 90 - (int)(180.0*atanf((float)(y)/x)/pi); //range: [-pi/2, pi/2]
+  //atanf range: [-pi/2, pi/2]
+  if (y >= 0){
+    return (int)(180.0*atanf((float)(y)/x)/pi);
+  }
+  else {
+    return (int)(180.0*atanf((float)(y)/x)/pi) + 180;
+  }
 }
 
 int theta2(int x, int y, int z){
-  return 90 - (int)(180.0*acosf((float)(z)/sqrt(x*x+y*y+z*z))/pi); //range: [0,pi]
+  //acosf range: [0, pi]
+  return (int)(180.0*acosf((float)(z)/sqrt(x*x+y*y+z*z))/pi);
 }
